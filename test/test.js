579d3249fb0abd9973112c70f278c7b602889299
Amo Wu <amo260@gmail.com>
2015-04-09 17:08:09 +0800

    test pull request

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
       assert.equal(-1, [1,2,3].indexOf(0));
+      assert.equal(3, [1,2,3].indexOf(5));
     })
   })
 })
